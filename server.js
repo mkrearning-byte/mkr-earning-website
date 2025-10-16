5bd257939a8fd03d5348545c71d8d1ad85a3fbb8
mkrearning-byte <mkr.earning@gmail.com>
2025-10-16 18:45:17 +0530

    Refactor server.js for Vercel compatibility

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 
 
 // === Import Routes ===
-const mkrRoutes = require('./routes/mkr');
+const mkrRoutes = require('./mkr.js');
 // === Use Routes ===
 // === Serve Frontend (index.html) ===
 app.use('/api', mkrRoutes);
@@ -65,5 +65,6 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
 
 
