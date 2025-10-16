8ff8c62e910e5f94b776d255a83ba28263e10059
mkrearning-byte <mkr.earning@gmail.com>
2025-10-16 18:47:18 +0530

    Refactor mkr.js to fix import paths and formatting

--- mkr.js
+++ mkr.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User =require('./user');
 
 const router = express.Router();
 
@@ -50,3 +50,4 @@ const resetDailyAdCount = (user) => {
 // ROUTE 1: Signup
 // ============================================
 router.post
+
